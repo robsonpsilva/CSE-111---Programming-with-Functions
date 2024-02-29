b66d32b2c94dbfb578529d69bfce906660daf85d
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-02-28 21:46:36 -0300

    modified:   Lesson2/tire_volume.py 	new file:   volumes.txt

--- Lesson2/tire_volume.py
+++ Lesson2/tire_volume.py
@@ -1,4 +1,5 @@
 import math
+from datetime import datetime
 
 """
 The size of a car tire in the United States is represented with three numbers like this: 205/60R15. The first number is the width of the tire in millimeters. The second number is the aspect ratio. The third number is the diameter in inches of the wheel that the tire fits. The volume of space inside a tire can be approximated with this formula:
@@ -11,13 +12,22 @@ w is the width of the tire in millimeters,
 a is the aspect ratio of the tire, and
 d is the diameter of the wheel in inches.
 """
+tyres_prices = [[185,65,15, 51.0],[205,60,15, 53.01],[175,70,14,39.12],[195,65,15,82.3]]
+
 try:
-    w = float(input('Enter the width of the tire in mm (ex 205):'))
-    a = float(input('Enter the aspect ratio of the tire (ex 60):'))
-    d = float(input('Enter the diameter of the wheel in inches (ex 15):'))
+    w = int(input('Enter the width of the tire in mm (ex 205):'))
+    a =int(input('Enter the aspect ratio of the tire (ex 60):'))
+    d = int(input('Enter the diameter of the wheel in inches (ex 15):'))
+    current_date_and_time = datetime.now()
 
     v = math.pi * (w ** 2) * a * (w * a + 2540 * d) / 10000000000
-
     print(f'The approximate volume is {v:.2f} liters')
+    for t in tyres_prices:
+        if t[0] == w and t[1] == a and t[2] == d:
+            print(f'The tyre prize is {t[3]:.2f}')
+
+    with open('volumes.txt', 'at') as volumes_file:
+        #Recording the file
+        print(f'{current_date_and_time:%Y-%m-%d}, {w}, {a}, {d}, {v:.2f}', file=volumes_file)
 except ValueError:
     print('Attention: Data entry is not a number!')
