8c4c9978d1f2817804110ba7f9c401b204b36cc7
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-03-14 17:30:42 -0300

    new file:   Lesson6/esteem.py 	renamed:    Lesson7/example.py -> Lesson6/example.py

---
+++ Lesson6/esteem.py
@@ -0,0 +1,30 @@
+"""
+The Rosenberg self-esteem scale is a self-esteem measure developed by the sociologist Morris Rosenberg in 1965. It is still used in social-science research today. To complete the measure, a person completes a survey that contains the following ten statements.
+
+I feel that I am a person of worth, at least on an equal plane with others.
+I feel that I have a number of good qualities.
+All in all, I am inclined to feel that I am a failure.
+I am able to do things as well as most other people.
+I feel I do not have much to be proud of.
+I take a positive attitude toward myself.
+On the whole, I am satisfied with myself.
+I wish I could have more respect for myself.
+I certainly feel useless at times.
+At times I think I am no good at all.
+
+This program is an implementation of the Rosenberg
+Self-Esteem Scale. This program will show you ten
+statements that you could possibly apply to yourself.
+"""
+
+def main():
+    print('This program is an implementation of the Rosenberg\
+    Self-Esteem Scale. This program will show you ten\
+    statements that you could possibly apply to yourself.\
+    Please rate how much you agree with each of the\
+    statements by responding with one of these four letters:')
+
+    print('D means you strongly disagree with the statement.\n\
+          d means you disagree with the statement.\n\
+          a means you agree with the statement.\n\
+          A means you strongly agree with the statement.')
