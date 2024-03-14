9c13642c4da86cee3624e6cff4e6c4ffa82f5085
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-03-14 16:21:16 -0300

    new file:   Lesson5/__pycache__/test_water_flow.cpython-312-pytest-8.1.1.pyc 	new file:   Lesson5/__pycache__/water_flow.cpython-312.pyc 	new file:   Lesson5/test_water_flow.py 	new file:   Lesson5/water_flow.py

---
+++ Lesson5/water_flow.py
@@ -0,0 +1,13 @@
+
+def water_column_height(tower_height, tank_height):
+    height = tower_height + 3 * tank_height / 4
+    return height
+
+def pressure_gain_from_water_height(height):
+    pressure = 998.2 * 9.80665 * height / 1000
+    return pressure
+
+def pressure_loss_from_pipe(pipe_diameter,
+        pipe_length, friction_factor, fluid_velocity):
+    pressure_loss = - friction_factor * pipe_length * 998.2 * (fluid_velocity ** 2) / (2000 * pipe_diameter)
+    return pressure_loss
