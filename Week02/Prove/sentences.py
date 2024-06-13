b1f70e7648a68117486680108a0446f2a639edb1
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-06-13 10:12:47 -0300

    renamed:    Week02/sentences.py -> Week02/Milestone/sentences.py 	new file:   Week02/Prove/sentences.py

---
+++ Week02/Prove/sentences.py
@@ -0,0 +1,200 @@
+import random
+
+# Create a list of strings and assign
+# the list to a variable named words.
+words = ["boy", "girl", "cat", "dog", "bird", "house"]
+
+# Call the random.choice function which will choose
+# one string from the words list. Store the chosen
+# string in a variable named word.
+word = random.choice(words)
+
+def get_determiner(quantity):
+    """Return a randomly chosen determiner. A determiner is
+    a word like "the", "a", "one", "some", "many".
+    If quantity is 1, this function will return either "a",
+    "one", or "the". Otherwise this function will return
+    either "some", "many", or "the".
+
+    Parameter
+        quantity: an integer.
+            If quantity is 1, this function will return a
+            determiner for a single noun. Otherwise this
+            function will return a determiner for a plural
+            noun.
+    Return: a randomly chosen determiner.
+    """
+    if quantity == 1:
+        words = ["a", "one", "the"]
+    else:
+        words = ["some", "many", "the"]
+
+    # Randomly choose and return a determiner.
+    word = random.choice(words)
+    return word
+
+def get_noun(quantity):
+    """Return a randomly chosen noun.
+    If quantity is 1, this function will
+    return one of these ten single nouns:
+        "bird", "boy", "car", "cat", "child",
+        "dog", "girl", "man", "rabbit", "woman"
+    Otherwise, this function will return one of
+    these ten plural nouns:
+        "birds", "boys", "cars", "cats", "children",
+        "dogs", "girls", "men", "rabbits", "women"
+
+    Parameter
+        quantity: an integer that determines if
+            the returned noun is single or plural.
+    Return: a randomly chosen noun.
+    """
+    if quantity == 1:
+        words = ["bird", "boy", "car", "cat", "child",
+        "dog", "girl", "man", "rabbit", "woman"]
+    else:
+        words = ["birds", "boys", "cars", "cats", "children",
+        "dogs", "girls", "men", "rabbits", "women"]
+
+    # Randomly choose and return a determiner.
+    word = random.choice(words)
+    return word
+
+def get_verb(quantity, tense):
+    """Return a randomly chosen verb. If tense is "past",
+    this function will return one of these ten verbs:
+        "drank", "ate", "grew", "laughed", "thought",
+        "ran", "slept", "talked", "walked", "wrote"
+    If tense is "present" and quantity is 1, this
+    function will return one of these ten verbs:
+        "drinks", "eats", "grows", "laughs", "thinks",
+        "runs", "sleeps", "talks", "walks", "writes"
+    If tense is "present" and quantity is NOT 1,
+    this function will return one of these ten verbs:
+        "drink", "eat", "grow", "laugh", "think",
+        "run", "sleep", "talk", "walk", "write"
+    If tense is "future", this function will return one of
+    these ten verbs:
+        "will drink", "will eat", "will grow", "will laugh",
+        "will think", "will run", "will sleep", "will talk",
+        "will walk", "will write"
+
+    Parameters
+        quantity: an integer that determines if the
+            returned verb is single or plural.
+        tense: a string that determines the verb conjugation,
+            either "past", "present" or "future".
+    Return: a randomly chosen verb.
+    """
+    if tense == "past":
+        words = ["drank", "ate", "grew", "laughed", "thought",
+        "ran", "slept", "talked", "walked", "wrote"]
+    elif tense == "present" and quantity == 1:
+        words = ["drinks", "eats", "grows", "laughs", "thinks",
+        "runs", "sleeps", "talks", "walks", "writes"]
+    elif tense == "present" and quantity != 1:
+        words = ["drink", "eat", "grow", "laugh", "think",
+        "run", "sleep", "talk", "walk", "write"]
+    elif tense == "future":
+        words = ["will drink", "will eat", "will grow", "will laugh",
+        "will think", "will run", "will sleep", "will talk",
+        "will walk", "will write"]
+
+    # Randomly choose and return a determiner.
+    word = random.choice(words)
+    return word
+
+def get_adjective(): #Exceeding the Requirements
+    words = ["stunning", "lovely", "charming", "joyful", "adorable", "impressive", "fabulous", "magnificent", "splendid", "astonishing",
+             "good", "bad", "big", "small", "happy", "sad", "rich", "poor", "young", "old", "hot", "cold", "dry", "wet", "easy", "hard"]
+    
+    # Randomly choose and return an adjective
+    word = random.choice(words)
+    return word
+
+def get_preposition():
+    """Return a randomly chosen preposition
+    from this list of prepositions:
+        "about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"
+
+    Return: a randomly chosen preposition.
+    """
+    words = [ "about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"]
+    word = random.choice(words)
+    return word
+
+def get_prepositional_phrase(quantity):
+    """Build and return a prepositional phrase composed
+    of three words: a preposition, a determiner, and a
+    noun by calling the get_preposition, get_determiner,
+    and get_noun functions.
+
+    Parameter
+        quantity: an integer that determines if the
+            determiner and noun in the prepositional
+            phrase returned from this function should
+            be single or pluaral.
+    Return: a prepositional phrase.
+    """
+    determiner = get_determiner(quantity)
+    noum = get_noun(quantity)
+    preposition = get_preposition()
+
+    sentence = preposition + ' ' + determiner +  ' ' + noum 
+    return sentence
+
+def make_sentence(quantity, tense):
+    """Build and return a sentence with three words:
+    a determiner, a noun, and a verb. The grammatical
+    quantity of the determiner and noun will match the
+    number in the quantity parameter. The grammatical
+    quantity and tense of the verb will match the number
+    and tense in the quantity and tense parameters.
+    """
+    determiner = get_determiner(quantity)
+    noum = get_noun(quantity)
+    verb = get_verb(quantity, tense)
+    preprositional_phrase = get_prepositional_phrase(quantity) 
+    preprositional_phrase_1 = get_prepositional_phrase(quantity) #Exceeding the Requirements
+    adjective = get_adjective() #Exceeding the Requirements
+
+    #Exceeding the Requirements
+    sentence = determiner + ' ' + adjective + ' ' + noum + ' ' + preprositional_phrase_1 + ' ' + verb + ' ' + preprositional_phrase
+    sentence = sentence.capitalize()
+    
+    return sentence
+
+def main():
+    print(' ')
+    #past singular
+    print(make_sentence(1,'past'))
+
+    #present singular
+    print(make_sentence(1,'present'))
+    
+    #future singular
+    print(make_sentence(1,'future'))
+
+    #past plural
+    print(make_sentence(2,'past'))
+
+    #present pliral
+    print(make_sentence(2,'present'))
+
+    #future plural
+    print(make_sentence(2,'future'))
+
+    print(' ')
+   
+
+main()
