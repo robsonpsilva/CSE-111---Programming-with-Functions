f7b07feaa434cd465fa227f5d8447484e9eee0b5
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-07-16 15:38:05 -0300

    modified:   Final_Project/posseidon_main.py

--- Final_Project/posseidon_main.py
+++ Final_Project/posseidon_main.py
@@ -8,28 +8,31 @@ class Screen:
         #Creating screen elements
         self.label1 = tk.Label(self.main_screen, text="Choose one of the options below")
        
-        r = tk.Radiobutton(
+        selected_opt = tk.StringVar()
+        self.r = tk.Radiobutton(
             master,
             text= 'List products and prices',
             value= 1,
             variable=selected_opt
-        )
-        r = tk.Radiobutton(
+        )   
+        self.r2 = tk.Radiobutton(
             master,
             text= 'Include/Exclude/Update an item in your request',
             value= 2,
             variable=selected_opt
         )
-        r = ttk.Radiobutton(
-            root,
+        self.r3 = tk.Radiobutton(
+            master,
             text= 'Include/Exclude/Update an item in your product list',
             value= 3,
             variable=selected_opt
         )
 
-
+        #Positioning widgets on screen
         self.label1.pack(fill='x', padx=5, pady=5, side=tk.LEFT)
-        r.pack(fill='x', padx=20, pady=5, side=tk.LEFT)
+        self.r1.pack(fill='x', padx=20, pady=5, side=tk.LEFT)
+        self.r2.pack(fill='x', padx=20, pady=5, side=tk.LEFT)
+        self.r3.pack(fill='x', padx=20, pady=5, side=tk.LEFT)
 
 #Creating our root interface
 
