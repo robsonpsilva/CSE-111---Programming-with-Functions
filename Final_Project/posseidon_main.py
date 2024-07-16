40d1f0492821def007ee0d175a9a50a912b5fcb9
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-07-16 15:02:17 -0300

    modified:   Final_Project/posseidon_main.py

--- Final_Project/posseidon_main.py
+++ Final_Project/posseidon_main.py
@@ -4,6 +4,8 @@ class Screen:
     def __init__(self,master):
        self.main_screen = master
        self.main_screen.title('Pegasus - Store Front and Stock Management')
+       self.label = tk.Label(self.main_screen, text="Choose one of the options below")
+       self.label.pack(fill='x', padx=5, pady=5)
 
 #Creating our root interface
 
