51d4dbdca852821dde29c4d6fcb39b74a3c5597c
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-07-16 15:22:37 -0300

    modified:   Final_Project/posseidon_main.py

--- Final_Project/posseidon_main.py
+++ Final_Project/posseidon_main.py
@@ -2,10 +2,34 @@ import tkinter as tk
 
 class Screen:
     def __init__(self,master):
-       self.main_screen = master
-       self.main_screen.title('Pegasus - Store Front and Stock Management')
-       self.label = tk.Label(self.main_screen, text="Choose one of the options below")
-       self.label.pack(fill='x', padx=5, pady=5)
+        self.main_screen = master
+        self.main_screen.title('Pegasus - Store Front and Stock Management')
+
+        #Creating screen elements
+        self.label1 = tk.Label(self.main_screen, text="Choose one of the options below")
+       
+        r = tk.Radiobutton(
+            master,
+            text= 'List products and prices',
+            value= 1,
+            variable=selected_opt
+        )
+        r = tk.Radiobutton(
+            master,
+            text= 'Include/Exclude/Update an item in your request',
+            value= 2,
+            variable=selected_opt
+        )
+        r = ttk.Radiobutton(
+            root,
+            text= 'Include/Exclude/Update an item in your product list',
+            value= 3,
+            variable=selected_opt
+        )
+
+
+        self.label1.pack(fill='x', padx=5, pady=5, side=tk.LEFT)
+        r.pack(fill='x', padx=20, pady=5, side=tk.LEFT)
 
 #Creating our root interface
 
